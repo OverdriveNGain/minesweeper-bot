5b5f80629e0985e9c65319d34d71e64446a59000
jeremy <jeremyamon@gmail.com>
2021-08-14 07:41:17 +0800

    Added constants descriptions

--- constants.py
+++ constants.py
@@ -1,7 +1,14 @@
+# These values may not work for some window sizes, monitor resolutions, or other websites.
+
 # https://minesweeperonline.com/ maximized to the left
+
+# Upper left corner of the mines area
 boardMinPoint = (89,161)
+# Lower right corner of the mines area
 boardMaxPoint = (569,417)
+# Lower right corner of the upper left most cell
 boardFirstCell = (105, 177)
+# Reset button location
 resetButtonAt = (330, 136)
 mineCount = 99
 boardColors = (189, 189, 189)
@@ -23,8 +30,6 @@ boardPixelWidth = boardMaxPoint[0] - boardMinPoint[0]
 boardPixelHeight = boardMaxPoint[1] - boardMinPoint[1]
 boardWidth = round(boardPixelWidth / (boardFirstCell[0] - boardMinPoint[0]))
 boardHeight = round(boardPixelHeight / (boardFirstCell[1] - boardMinPoint[1]))
-print(boardWidth)
-print(boardHeight)
 cellWidth = boardPixelWidth / boardWidth
 cellHeight = boardPixelHeight / boardHeight
 testingCellWidth = cellWidth - cellTestingPadding * 2
